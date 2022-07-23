68b845aade535b370ca1ccd3ef43a0fb1a3babf1
AkshathSai <akshathsai.pittala@outlook.com>
2022-07-23 11:07:41 +0530

    Added Structure for Account and unified both Savings & Current Accounts in a single module

---
+++ Account.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct accountDetails {
+
+    long accountNumber;
+    char firstName[1000];
+    char lastName[1000];
+    char address[1000];
+    float balance;
+    char accountType[100];
+
+} account;
+
+void accountOpening(){
+
+    int userCase;
+    //Pseudo random number generator
+    time_t t;
+    /* Intializes random number generator */
+    srand((unsigned) time(&t));
+
+    //Structure object
+    account acc;
+
+    printf("Enter your Details to 1.savings account 2.current Account\n");
+    scanf("%d", &userCase);
+
+    if (userCase == 1 || 2)
+    {
+
+        FILE *fp=fopen("AccountDetails.txt","a+");
+
+        //Assigning auto generated number as account number
+        acc.accountNumber = rand() % 9999;
+
+        printf("Enter your firstName:\n");
+        gets(acc.firstName);
+        printf("Enter your lastname:\n");
+        gets(acc.lastName);
+        printf("Enter your address:\n");
+        gets(acc.address);
+        printf("Enter your Balance:\n");
+        scanf("%f", &acc.balance);
+        printf("Enter your Account Type:\n");
+        gets(acc.accountType);
+
+        fprintf(fp,"%s", acc.accountNumber);
+        fprintf(fp,"%s", acc.firstName);
+        fprintf(fp,"%s", acc.lastName);
+        fprintf(fp,"%s", acc.address);
+        fprintf(fp,"%s", acc.balance);
+        fprintf(fp,"%s", acc.accountType);    
+
+        printf("Your Account details\n");
+        fscanf(fp, "%d", &acc.accountNumber);
+        printf("Account Number : %d", acc.accountNumber);
+        // printf("First Name : %d", acc.firstName);
+        // printf("Last Name : %d", acc.lastName);
+        // printf("Adress : %d", acc.address);
+        // printf("Balance : %d", acc.balance);
+        // printf("Account Type : %d", acc.accountType);
+
+        fclose(fp);
+
+    } else {
+        printf("Invalid Input");
+        accountOpening();
+    }
+
+}
+
+
+
+
+
+
+
+
--- Main.c
+++ Main.c
@@ -1,15 +1,16 @@
-
 #include<stdio.h>
 #include <stdlib.h>
-#include "Banking.c"
+#include "Account.c"
+
 int main(){
+
     int userCase;
-printf(" Welcome to ABC Banking!!!\n Choose the below options to proceed further.\n 1. Savings Account Opening\n 2. Current Account Opening\n 3. Interest rate calculation\n 4. Check balance. \n 5. Print Mini Statement\n 6. Withdrawl \n 7.Deposit. \n 8.Exit.\n");
-scanf("%d",&userCase);
+    printf("Welcome to ABC Banking!!!\n Choose the below options to proceed further.\n 1. Savings Account Opening\n 2. Current Account Opening\n 3. Interest rate calculation\n 4. Check balance. \n 5. Print Mini Statement\n 6. Withdrawl \n 7.Deposit. \n 8.Exit.\n");
+    scanf("%d",&userCase);
 
     switch(userCase){
     case 1:
-     savingsAccount();
+     accountOpening();
      break;
      case 2:
      break;
